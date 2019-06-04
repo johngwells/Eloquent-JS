b656724ddc4bdc9ee6d6740d05b3b12e5507b642
John Wells <molekewl2017@gmail.com>
2019-06-03 21:40:15 -0700

    completed excersice 1 Looping a triangle

---
+++ LoopingATriangle.js
@@ -0,0 +1,20 @@
+// Loop that makes 7 calls to console.log
+/*
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+function triangle(n) {
+  let tri = '';
+
+  for (let i = 0; i < n; i++) {
+    console.log(tri += '#');
+  }
+}
+
+triangle(7);
