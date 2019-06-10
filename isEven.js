1a87de8f16e7e0704def2fddd5a78b96d9558e24
John Wells <molekewl2017@gmail.com>
2019-06-10 00:28:03 -0700

    exercise completed

---
+++ isEven.js
@@ -0,0 +1,24 @@
+// Excersice - Recursion - the number is even or odd. Address negative numbers
+// as well as you can't use %
+
+function isEven(num) {
+  if (num === 0) {
+    return true;
+  }
+  else if (num === 1) {
+    return false;
+  }
+  // turn negative to positive
+  else if (num < 0) {
+    return isEven(-num);
+  } 
+  // subtracts to get 1 or 0
+  else {
+    return isEven(num - 2);
+  }
+}
+
+console.log(isEven(-2));
+console.log(isEven(1));
+console.log(isEven(-5));
+console.log(isEven(10));
