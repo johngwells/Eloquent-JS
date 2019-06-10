c23e9cdbea718e8763609d4d0c9c0159395a5110
John Wells <molekewl2017@gmail.com>
2019-06-10 00:07:27 -0700

    completed Exercise

--- Minimum.js
+++ Minimum.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 // Exercise - Minimum - Create a function like Math.min
+=======
+// Excersice - Minimum - Create a function like Math.min
+>>>>>>> 1f4ef1f... completed Excersise
 
 function min(num1, num2) {
   if (num1 < num2) {
