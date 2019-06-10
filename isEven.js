5cc18ec1c11f460867d0a6211b6d2092fc7208fe
John Wells <molekewl2017@gmail.com>
2019-06-10 00:28:03 -0700

    exercise completed

--- isEven.js
+++ isEven.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 // Exercise - Recursion - the number is even or odd. Address negative numbers
+=======
+// Excersice - Recursion - the number is even or odd. Address negative numbers
+>>>>>>> c6e9587... excercise completed
 // as well as you can't use %
 
 function isEven(num) {
