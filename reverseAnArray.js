02e43b4e94a638819c6015d73b2f9a2182b4cdad
John Wells <molekewl2017@gmail.com>
2019-06-16 17:25:03 -0700

    exercise complete

--- reverseAnArray.js
+++ reverseAnArray.js
@@ -6,6 +6,7 @@ does what the reverse method does: it modifies the array given as argument by
 reversing its elements. Neither may use the standard reverse method.
 */
 
+// Exercise 1 - Array out
 function reverseArray(array) {
   var newArray = [];
   for (let i = array.length - 1; i >= 0; i--) {
@@ -14,5 +15,21 @@ function reverseArray(array) {
   return newArray;
 }
 
-console.log(reverseArray([1, 2, 3, 4, 5]));
-console.log(reverseArray(['if', 'maybe', 'so', 'never', 'yes']));
+// console.log(reverseArray([1, 2, 3, 4, 5]));
+// console.log(reverseArray(['if', 'maybe', 'so', 'never', 'yes']));
+
+// Exercise 2 - Array In
+function reverseArrayInPlace(array) {
+  for (var i = 0; i <= Math.floor((array.length - 1) / 2); i++) {
+    // set a variable equal to the first
+    let firstElement = array[i];
+    // set the first element equal to the last
+    array[i] = array[array.length - 1 - i];
+    // last element equal to the first;
+    array[array.length - 1 - i] = firstElement;
+  }
+  return array;
+}
+
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
+console.log(reverseArrayInPlace(['if', 'maybe', 'so', 'never', 'yes']));
